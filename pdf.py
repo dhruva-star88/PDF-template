0ddec080f80a07fb0ea03a5cd2a4b236493d8881
dhruva-star88 <dhruvatej6565@gmail.com>
2024-02-16 17:46:13 +0530

    Added Footer

--- pdf.py
+++ pdf.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 data = pd.read_csv("pdf_topics.csv")
 
@@ -18,9 +19,21 @@ for index, row in data.iterrows():
     # Here arguments of line are x1,y1,x2,y2 => x1,y1 represents starting point and x2,y2, represents ending point
     pdf.line(10, 21, 200, 21)
 
-    # This is the nested loop, we itirate over the pages to add the particular number of pages as in csv data
-    # We substract from 1, cuz we already have one page in the above parent itiration
+    # To set Footer, First we need to break the line to add text below
+    pdf.ln(262)
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
+    # This is the nested loop, we iterate over the pages to add the particular number of pages as in csv data
+    # We subtract from 1, cuz we already have one page in the above parent iteration
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # To set footer for children pages
+        pdf.ln(274)
+        pdf.set_font(family="Times", style="B", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
 
 pdf.output("notes.pdf")
